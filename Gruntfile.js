2234a6f456a4b2155402890d1ab72f8bb56519e0
Rob Denton <robdentonrg@gmail.com>
2017-04-13 19:24:10 -0700

    blah

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,48 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		now: grunt.template.today('yyyymmdd'),
+		ver: 1,
+		/* WATCH */
+		watch: {
+			tmpl: {
+				files: [
+					'./build/*'
+				],
+				tasks: ['dev']
+			}
+		},
+		/* JSHINT */
+		jshinta: {
+			
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				src: 'src/<%= pkg.name %>.js',
+				dest: 'build/<%= pkg.name %>.min.js'
+			}
+		}
+	});
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-env');
+	grunt.loadNpmTasks('grunt-preprocess');
+	grunt.loadNpmTasks('leaflet');
+	grunt.loadNpmTasks('leaflet-omnivore');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('dev', 
+	// Default task(s).
+	grunt.registerTask('default', ['uglify']);
 
 };
